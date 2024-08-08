8d8ce326a42cd4bca24b61dc3c9088cd2ced8c42
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2024-08-08 17:17:15 +0800

    Update FlaskPassGen.py

--- FlaskPassGen.py
+++ FlaskPassGen.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import random
 import string
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def index():
     return render_template('index.html', password=password)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment, default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
